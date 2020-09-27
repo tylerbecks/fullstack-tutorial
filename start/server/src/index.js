db1ef301e91331e2bbc1ee68e000af3491480a8b
Tyler Becks <tylerdbecks@gmail.com>
2020-09-27 13:53:11 -0700

    Set up Apollo Client
    Step 6/9 of tutorial

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -15,9 +15,7 @@ const server = new ApolloServer({
   context: async ({ req }) => {
     // simple auth check on every request
     const auth = (req.headers && req.headers.authorization) || '';
-    console.log(req.headers)
     const email = Buffer.from(auth, 'base64').toString('ascii');
-    console.log({  email  });
     if (!isEmail.validate(email)) return { user: null };
     // find a user by their email
     const users = await store.users.findOrCreate({ where: { email } });
